714aa52ac54ae73dbfb122aa5231751e1bf652b5
Ru <144015384+rubyyu05241024@users.noreply.github.com>
2023-09-19 19:11:50 +0800

    Update 13577 - Anya loves to encrypt - Ru.c

--- 2832- I2P(I)2023_Hu_Hw2/13577 - Anya loves to encrypt - Ru.c
+++ 2832- I2P(I)2023_Hu_Hw2/13577 - Anya loves to encrypt - Ru.c
@@ -4,7 +4,7 @@ int main(void){
     char s[5];
     scanf("%s",s);
     for(int i=0;i<5;i++){
-        s[i] = 187-s[i];
+        s[i] = 187-s[i];    //90-(ascii-32-65)
     }
     printf("%s\n",s);
     return 0;
